c52a7d3620ad3ebfbfaa22655ea1512293c9f52b
[Shagun Nirvan] <[nirvanshagun173@gmail.com]>
2025-06-19 10:33:11 +0530

    new

---
+++ Array/ArrayCC.java
@@ -0,0 +1,12 @@
+package Array;
+import java.util.*;
+
+public class ArrayCC {
+    public static void main(String[] args) {
+        int marks[] = new int[50];
+
+        int numbers[] = {1,2,3}; //if size is not initiated then java will automatically initiate the size during runtime on the basis of inputs in the array
+        
+         
+    }
+}
